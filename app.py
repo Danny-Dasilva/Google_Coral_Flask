28e0e211e98ae52c327e68324e7980ac403737b3
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-08 00:39:10 -0400

    www

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from teachable import main
 import sys
 from multiprocessing import Process
 import time
-
+from flask import Flask, render_template
+app = Flask(__name__)
 
 # def f():
 #     sys.exit(main(sys.argv))
@@ -12,49 +13,28 @@ import time
 #     run_server(render_gen)
 
 
+@app.route('/')
+def index():
+    return render_template('layout.html')
 
-# if __name__ == '__main__':
-#     sys.exit(main(sys.argv))
 
-#     # global p
-#     # p = Process(target=f)
-#     # p.start()
+
+def a():
+    app.run(host='0.0.0.0', debug=False)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
+    global p
+    p = Process(target=a)
+    p.start()
 
 #     global r
 #     r = Process(target=l)
 #     r.start()
     
 
-from flask import Flask, render_template, Response
-import threading
-from flask import Flask
-from celery import Celery
-
-app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('layout.html')
-
-
-@celery.task
-def my_background_task():
-    sys.exit(main(sys.argv))
-
-task = my_background_task.apply_async()
-    
 
 
 
-if __name__ == '__main__':
-    
-    app.run(host='0.0.0.0', debug=False)
 
-    
