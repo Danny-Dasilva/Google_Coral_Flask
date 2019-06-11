846d7afdc85aeba77ac86a58a7e658c1f3e64268
tech-garage <yahchayildasilva@gmail.com>
2019-06-11 12:10:38 -0400

    fps demo

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import cv2
 from flask import Flask, render_template, Response
+import datetime
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
@@ -21,6 +22,70 @@ class VideoCamera(object):
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
+class FPS:
+	def __init__(self):
+		# store the start time, end time, and total number of frames
+		# that were examined between the start and end intervals
+		self._start = None
+		self._end = None
+		self._numFrames = 0
+ 
+	def start(self):
+		# start the timer
+		self._start = datetime.datetime.now()
+		return self
+ 
+	def stop(self):
+		# stop the timer
+		self._end = datetime.datetime.now()
+ 
+	def update(self):
+		# increment the total number of frames examined during the
+		# start and end intervals
+		self._numFrames += 1
+ 
+	def elapsed(self):
+		# return the total number of seconds between the start and
+		# end interval
+		return (self._end - self._start).total_seconds()
+ 
+	def fps(self):
+		# compute the (approximate) frames per second
+		return self._numFrames / self.elapsed()
+
+class WebcamVideoStream:
+	def __init__(self, src=0):
+		# initialize the video camera stream and read the first frame
+		# from the stream
+		self.stream = cv2.VideoCapture(src)
+		(self.grabbed, self.frame) = self.stream.read()
+ 
+		# initialize the variable used to indicate if the thread should
+		# be stopped
+		self.stopped = False
+    def start(self):
+		# start the thread to read frames from the video stream
+		Thread(target=self.update, args=()).start()
+		return self
+ 
+	def update(self):
+		# keep looping infinitely until the thread is stopped
+		while True:
+			# if the thread indicator variable is set, stop the thread
+			if self.stopped:
+				return
+ 
+			# otherwise, read the next frame from the stream
+			(self.grabbed, self.frame) = self.stream.read()
+ 
+	def read(self):
+		# return the frame most recently read
+		return self.frame
+ 
+	def stop(self):
+		# indicate that the thread should be stopped
+		self.stopped = True
+        
 
 app = Flask(__name__)
 
