5d2d5291891cfe64d2334fb758df2d440eac8b1c
tech-garage <yahchayildasilva@gmail.com>
2019-06-11 14:29:11 -0400

    changed port

--- teachable.py
+++ teachable.py
@@ -41,7 +41,7 @@ print("Socket successfully created")
   
 # reserve a port on your computer in our 
 # case it is 12345 but it can be anything 
-port = 12345                
+port = 14445                
   
 # Next bind to the port 
 # we have not typed any ip in the ip field 
