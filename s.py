ac6cd377a7425486f1a49771b8512e5322410285
tech-garage <yahchayildasilva@gmail.com>
2019-06-11 14:01:36 -0400

    FIXED PRINT STATEMENTS

--- s.py
+++ s.py
@@ -4,7 +4,7 @@ import socket
   
 # next create a socket object 
 s = socket.socket()          
-print "Socket successfully created"
+print("Socket successfully created")
   
 # reserve a port on your computer in our 
 # case it is 12345 but it can be anything 
@@ -16,11 +16,11 @@ port = 12345
 # this makes the server listen to requests  
 # coming from other computers on the network 
 s.bind(('', port))         
-print "socket binded to %s" %(port) 
+print ("socket binded to %s" %(port) )
   
 # put the socket into listening mode 
 s.listen(5)      
-print "socket is listening"            
+print ("socket is listening" )           
   
 # a forever loop until we interrupt it or  
 # an error occurs 
@@ -28,7 +28,7 @@ while True:
   
    # Establish connection with client. 
    c, addr = s.accept()      
-   print 'Got connection from', addr 
+   print ('Got connection from', addr )
   
    # send a thank you message to the client.  
    c.send('Thank you for connecting') 
