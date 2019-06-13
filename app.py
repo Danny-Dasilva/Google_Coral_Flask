ad06864597a9314505170370f951faee3d16eed5
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-12 21:17:27 -0400

    3

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1235))
 
 def a():
-    while True:
-        msg = s.recv(1024)
-        print(msg.decode("utf-8"))
+
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
     
     
 
