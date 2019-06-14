58ee7e5ff4d018f44821e08c336bf966e52ffa97
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-14 17:17:16 -0400

    time

--- c.py
+++ c.py
@@ -1,8 +1,10 @@
 import socket
-for i in range(10):
+import time
+while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((socket.gethostname(), 1234))
 
     while True:
         msg = s.recv(8)
         print(msg.decode("utf-8"))
+    time.sleep(2)
