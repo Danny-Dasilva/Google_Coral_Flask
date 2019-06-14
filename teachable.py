150f94a8777be1127c9cf6198fd9806dcaba1f02
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-14 16:36:40 -0400

    removed status

--- teachable.py
+++ teachable.py
@@ -246,10 +246,10 @@ class TeachableMachine(object):
     status = 'fps %.1f; #examples: %d; Class % 7s'%(
             fps, self._engine.exampleCount(),
             classes[classification or 0])
-    l = print(status)
+  
     svg.add(svg.text(status, insert=(26, 26), fill='black', font_size='20'))
     svg.add(svg.text(status, insert=(25, 25), fill='white', font_size='20'))
-    #return l
+   
     
     
 
