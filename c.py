ccb7e342ca3493c8ce4a5e7da819bc7c56eef094
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-14 17:12:37 -0400

    changed client

--- c.py
+++ c.py
@@ -1,8 +1,8 @@
 import socket
+for i in range(10):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 1234))
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-
-while True:
-    msg = s.recv(8)
-    print(msg.decode("utf-8"))
+    while True:
+        msg = s.recv(8)
+        print(msg.decode("utf-8"))
