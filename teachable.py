74652787f4aa13178f6577466c3e6ea34ad3e724
tech-garage <yahchayildasilva@gmail.com>
2019-06-17 13:12:45 -0400

    teachable print statements

--- teachable.py
+++ teachable.py
@@ -246,15 +246,10 @@ class TeachableMachine(object):
     clientsocket = self.clientsocket  
     
     # print("Connection from has been established!", address)
-    print(img.format)
-    print("mode", img.mode)
-    print(img.size)
-    print(img.palette)
-    
+  
     #clientsocket.send(bytes(status, "utf-8"))
     image = img.tobytes()
     # print(list(image))
-    print(sys.getsizeof(image))
     clientsocket.sendall(image)
 
     svg.add(svg.text(status, insert=(26, 26), fill='black', font_size='20'))
