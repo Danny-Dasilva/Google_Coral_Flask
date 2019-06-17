b3cdb8db3aa0946b4baa5ba3210979c832d7c543
tech-garage <yahchayildasilva@gmail.com>
2019-06-17 09:58:22 -0400

    d

--- teachable.py
+++ teachable.py
@@ -249,7 +249,7 @@ class TeachableMachine(object):
     
     # clientsocket.send(bytes(img, "utf-8"))
     image = img.tobytes()
-    clientsocket.sendall(image)
+    clientsocket.sendall(status)
 
     svg.add(svg.text(status, insert=(26, 26), fill='black', font_size='20'))
     svg.add(svg.text(status, insert=(25, 25), fill='white', font_size='20'))
