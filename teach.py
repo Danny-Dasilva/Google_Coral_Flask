b484e681282b1d0c900d5994819a24c675bb9a8a
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-19 14:37:20 -0400

    2

--- teach.py
+++ teach.py
@@ -56,15 +56,58 @@ class UI(object):
    
     current_time = time.time()
 
-class UI_Keyboard(UI):
-  def __init__(self):
-    global keyinput
-    import keyinput
 
-    # Layout of GPIOs for Raspberry demo
-    self._buttons = ['q', '1', '2', '3', '4']
-    self._LEDs = [None]*5
-    super(UI_Keyboard, self).__init__()
+class UI(object):
+  """Abstract UI class. Subclassed by specific board implementations."""
+  def __init__(self):
+    self._button_state = [False for _ in self._buttons]
+    current_time = time.time()
+    self._button_state_last_change = [current_time for _ in self._buttons]
+    self._debounce_interval = 0.1 # seconds
+
+  def setOnlyLED(self, index):
+    for i in range(len(self._LEDs)): self.setLED(i, False)
+    if index is not None: self.setLED(index, True)
+
+  def isButtonPressed(self, index):
+    buttons = self.getButtonState()
+    return buttons[index]
+
+  def setLED(self, index, state):
+    raise NotImplementedError()
+
+  def getButtonState(self):
+    raise NotImplementedError()
+
+  def getDebouncedButtonState(self):
+    t = time.time()
+    for i,new in enumerate(self.getButtonState()):
+      if not new:
+        self._button_state[i] = False
+        continue
+      old = self._button_state[i]
+      if ((t-self._button_state_last_change[i]) >
+             self._debounce_interval) and not old:
+        self._button_state[i] = True
+      else:
+        self._button_state[i] = False
+      self._button_state_last_change[i] = t
+    return self._button_state
+
+  def testButtons(self):
+    while True:
+      for i in range(5):
+        self.setLED(i, self.isButtonPressed(i))
+      print('Buttons: ', ' '.join([str(i) for i,v in
+          enumerate(self.getButtonState()) if v]))
+      time.sleep(0.01)
+
+  def wiggleLEDs(self, reps=3):
+    for i in range(reps):
+      for i in range(5):
+        self.setLED(i, True)
+        time.sleep(0.05)
+        self.setLED(i, False)
 
 
 
