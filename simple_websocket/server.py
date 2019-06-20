17b1e52bb2365df9dd2146bc48d80b9b8a31724e
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-20 10:39:19 -0400

    no socke

--- simple_websocket/server.py
+++ simple_websocket/server.py
@@ -1,11 +1,11 @@
-from Cam import camera
+# from Cam import camera
 from flask import Flask, send_file, Response, render_template
 
-Image = camera()
+#Image = camera()
 app = Flask(__name__)
-@app.route('/video_feed')
-def video_feed():
-    return Response(Image.ImageStream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(Image.ImageStream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/')
 def index():
     return 'functional'
