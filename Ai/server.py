d3452621388fc45fc5fe27646e937516d6558f48
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-27 20:16:55 +0000

    modified:   Ai/server.py

--- Ai/server.py
+++ Ai/server.py
@@ -1,18 +1,19 @@
 from Cam import camera
 from flask import Flask, send_file, Response, render_template
 import teach
+from time import sleep
 import Image_classify
 import face_detect
 from threading import Thread
 from UpdateString import RandomThread
 import sys
-#from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit
 from time import sleep
-#kit = ServoKit(channels = 16)
+kit = ServoKit(channels = 16)
 
 app = Flask(__name__)
-#Image = camera(teach.AI())
-Image = camera(Image_classify.AI())
+Image = camera(teach.AI())
+#Image = camera(Image_classify.AI())
 #Image = camera(face_detect.AI())
 @app.route('/')
 def index():
@@ -33,9 +34,18 @@ if __name__ == "__main__":
     while True:
         sleep(0.01)
         result = Image.val
+        
         if(result == "One"):
             print("One")
-            #kit.servo[0].angle = 0
+            kit.servo[0].angle = 0
+            sleep(0.4)
+            #kit.continuous_servo[1].throttle = 0.3
+            kit.servo[1].angle = 0
         elif(result == "Two"):
             print("Two")
-            #kit.servo[0].angle = 180
+            kit.servo[0].angle = 30
+            sleep(0.4)
+            kit.servo[1].angle = 0
+        else:
+            kit.servo[1].angle = 17
+
