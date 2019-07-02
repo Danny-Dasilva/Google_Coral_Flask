fce29564c71cff82827a8549cffa921db4b3a9c9
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-07-02 18:47:02 +0000

    new file:   Drone/Cam.py 	new file:   Drone/Gentona-Bold.ttf 	new file:   Drone/GoDrone.py 	new file:   Drone/Image_classify.py 	new file:   Drone/__pycache__/AI.cpython-35.pyc 	new file:   Drone/__pycache__/Cam.cpython-35.pyc 	new file:   Drone/__pycache__/GoDrone.cpython-35.pyc 	new file:   Drone/__pycache__/Image_classify.cpython-35.pyc 	new file:   Drone/__pycache__/MJPEGStream.cpython-35.pyc 	new file:   Drone/__pycache__/UpdateString.cpython-35.pyc 	new file:   Drone/__pycache__/embedding.cpython-35.pyc 	new file:   Drone/__pycache__/face_detect.cpython-35.pyc 	new file:   Drone/__pycache__/gstreamer.cpython-35.pyc 	new file:   Drone/__pycache__/keyinput.cpython-35.pyc 	new file:   Drone/__pycache__/teach.cpython-35.pyc 	new file:   Drone/all_models/coco_labels.txt 	new file:   Drone/all_models/imagenet_labels.txt 	new file:   Drone/all_models/inat_bird_labels.txt 	new file:   Drone/all_models/inat_insect_labels.txt 	new file:   Drone/all_models/inat_plant_labels.txt 	new file:   Drone/all_models/inception_v1_224_quant.tflite 	new file:   Drone/all_models/inception_v1_224_quant_edgetpu.tflite 	new file:   Drone/all_models/inception_v2_224_quant.tflite 	new file:   Drone/all_models/inception_v2_224_quant_edgetpu.tflite 	new file:   Drone/all_models/inception_v3_299_quant.tflite 	new file:   Drone/all_models/inception_v3_299_quant_edgetpu.tflite 	new file:   Drone/all_models/inception_v4_299_quant.tflite 	new file:   Drone/all_models/inception_v4_299_quant_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_ssd_v1_coco_quant_postprocess.tflite 	new file:   Drone/all_models/mobilenet_ssd_v1_coco_quant_postprocess_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_ssd_v2_coco_quant_postprocess.tflite 	new file:   Drone/all_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_ssd_v2_face_quant_postprocess.tflite 	new file:   Drone/all_models/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite.1 	new file:   Drone/all_models/mobilenet_v1_1.0_224_quant.tflite 	new file:   Drone/all_models/mobilenet_v1_1.0_224_quant_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_v1_1.0_224_quant_embedding_extractor.tflite 	new file:   Drone/all_models/mobilenet_v1_1.0_224_quant_embedding_extractor_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_inat_bird_quant.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_inat_insect_quant.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_inat_insect_quant_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_inat_plant_quant.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_inat_plant_quant_edgetpu.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_quant.tflite 	new file:   Drone/all_models/mobilenet_v2_1.0_224_quant_edgetpu.tflite 	new file:   Drone/all_models/pet_labels.txt 	new file:   Drone/app.py 	new file:   Drone/embedding.py 	new file:   Drone/face_detect.py 	new file:   Drone/gstreamer.py 	new file:   Drone/gstreamer/README.md 	new file:   Drone/gstreamer/classify.py 	new file:   Drone/gstreamer/detect.py 	new file:   Drone/gstreamer/gstreamer.py 	new file:   Drone/gstreamer/install_requirements.sh 	new file:   Drone/install.sh 	new file:   Drone/keyinput.py 	new file:   Drone/mobilenet_quant_v1_224_headless_edgetpu.tflite 	new file:   Drone/server.py 	new file:   Drone/teach.py 	new file:   Drone/templates/index.html

---
+++ Drone/server.py
@@ -0,0 +1,57 @@
+from Cam import camera
+from flask import Flask, send_file, Response, render_template
+import teach
+import keyboard
+from time import sleep
+import Image_classify
+import face_detect
+from GoDrone import drone
+
+from threading import Thread, active_count
+import signal
+from threading import Thread
+
+import sys
+#from adafruit_servokit import ServoKit
+from time import sleep
+#kit = ServoKit(channels = 16)
+
+app = Flask(__name__)
+#Image = camera(teach.AI())
+Drone = drone()
+#Image = camera(Image_classify.AI())
+Image = camera(face_detect.AI())
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(Image.ImageStream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def flaskServer():
+    app.run(host="0.0.0.0", debug=False)
+
+def signal_handler(signal, frame):
+    print("\nprogram exiting gracefully")
+    sys.exit()
+def Drone_code():
+    while True:
+        sleep(0.01)
+        #print(list(Drone.channels))
+        if(Drone.align == True):
+            Drone.sendFaceData(Image.result)
+            Drone.sendSBUSData(Drone.channels)
+        else:
+            Drone.sendSBUSData(Drone.channels)
+    
+
+if __name__ == "__main__":
+    thread = Thread(target=flaskServer)
+    thread.daemon = True
+    thread.start()
+    thread1 = Thread(target=Drone_code)
+    thread1.daemon = True
+    thread1.start()
+    sleep(2)
+    signal.signal(signal.SIGINT, signal_handler)
