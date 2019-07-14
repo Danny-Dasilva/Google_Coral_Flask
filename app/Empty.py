33328134faecfd9a083c9f1446de49daaa32d2f0
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-07-14 15:42:06 -0400

    2

--- app/Empty.py
+++ app/Empty.py
@@ -33,60 +33,16 @@ import imp
 import os
 from edgetpu.detection.engine import DetectionEngine
 from app import gstreamer
-flaskImage = None
-flaskStatus = None
-def load_labels(path):
-    p = re.compile(r'\s*(\d+)(.+)')
-    with open(path, 'r', encoding='utf-8') as f:
-       lines = (p.match(line).groups() for line in f.readlines())
-       return {int(num): text.strip() for num, text in lines}
-
-def shadow_text(dwg, x, y, text, font_size=20):
-    dwg.add(dwg.text(text, insert=(x+1, y+1), fill='black', font_size=font_size))
-    dwg.add(dwg.text(text, insert=(x, y), fill='white', font_size=font_size))
-
-def generate_svg(dwg, objs, labels, text_lines):
-    width, height = dwg.attribs['width'], dwg.attribs['height']
-    for y, line in enumerate(text_lines):
-        shadow_text(dwg, 10, y*20, line)
-    for obj in objs:
-        x0, y0, x1, y1 = obj.bounding_box.flatten().tolist()
-        x, y, w, h = x0, y0, x1 - x0, y1 - y0
-        x, y, w, h = int(x * width), int(y * height), int(w * width), int(h * height)
-        percent = int(100 * obj.score)
-        label = '%d%% %s' % (percent, labels[obj.label_id])
-        shadow_text(dwg, x, y - 5, label)
-        dwg.add(dwg.rect(insert=(x,y), size=(w, h),
-                        fill='red', fill_opacity=0.3, stroke='white'))
 
 class Model():
     def __init__(self):
-        default_model_dir = './app/all_models'
-        default_model = 'mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
-        default_labels = 'coco_labels.txt'
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--model', help='.tflite model path',
-                            default=os.path.join(default_model_dir,default_model))
-        parser.add_argument('--labels', help='label file path',
-                            default=os.path.join(default_model_dir, default_labels))
-        parser.add_argument('--top_k', type=int, default=3,
-                            help='number of classes with highest score to display')
-        parser.add_argument('--threshold', type=float, default=0.1,
-                            help='class score threshold')
-        self.args = parser.parse_args()
-
-        print("Loading %s with %s labels."%(self.args.model, self.args.labels))
-        self.engine = DetectionEngine(self.args.model)
-        self.labels = load_labels(self.args.labels)
 
         self.last_time = time.monotonic()
     def user_callback(self, image):
 
       #added
       start_time = time.monotonic()
-      objs = self.engine.DetectWithImage(image, threshold=self.args.threshold,
-                                    keep_aspect_ratio=True, relative_coord=True,
-                                    top_k=self.args.top_k)
+      
       end_time = time.monotonic()
       self.last_time = end_time
 
