f9efd93b9d46c0feb71d21c30bdbdd7f0522c3b1
yahchayildasilva@gmail.com <yahchayildasilva@gmail.com>
2019-08-24 00:04:35 +0000

    changes to path routing

--- app/Detect.py
+++ app/Detect.py
@@ -41,23 +41,7 @@ def load_labels(path):
        lines = (p.match(line).groups() for line in f.readlines())
        return {int(num): text.strip() for num, text in lines}
 
-def shadow_text(dwg, x, y, text, font_size=20):
-    dwg.add(dwg.text(text, insert=(x+1, y+1), fill='black', font_size=font_size))
-    dwg.add(dwg.text(text, insert=(x, y), fill='white', font_size=font_size))
 
-def generate_svg(dwg, objs, labels, text_lines):
-    width, height = dwg.attribs['width'], dwg.attribs['height']
-    for y, line in enumerate(text_lines):
-        shadow_text(dwg, 10, y*20, line)
-    for obj in objs:
-        x0, y0, x1, y1 = obj.bounding_box.flatten().tolist()
-        x, y, w, h = x0, y0, x1 - x0, y1 - y0
-        x, y, w, h = int(x * width), int(y * height), int(w * width), int(h * height)
-        percent = int(100 * obj.score)
-        label = '%d%% %s' % (percent, labels[obj.label_id])
-        shadow_text(dwg, x, y - 5, label)
-        dwg.add(dwg.rect(insert=(x,y), size=(w, h),
-                        fill='red', fill_opacity=0.3, stroke='white'))
 
 class Model():
     def __init__(self):
